9e42eeecae544dadce19b3805185243f9658fbd1
Anjana <anjanap1308@gmail.com>
2017-11-17 22:58:43 -0800

    code commit

---
+++ url/routes/generate.js
@@ -0,0 +1,27 @@
+var mongo = require("./mongo");
+var mongoURL = "mongodb://localhost:27017/hackaton";
+var shortUrl = require('node-url-shortener');
+var opn = require('opn');
+
+exports.generate = function(req, res){
+	var org=req.param("orgurl");
+	console.log("ORG: "+org);
+	shortUrl.short(org, function(err, url){
+		var newURL={org_url:org,short_url:url};
+	    mongo.connect(mongoURL, function(){
+	        console.log('Connected to mongo at: ' + mongoURL);
+	        var col1 = mongo.collection('url_details');
+	        col1.insertOne(newURL, function(err, result) {
+	            if (err) throw err;
+	            else{
+	            	console.log(url);
+	            	opn(org);
+	            	 res.render('index', { url: url });
+	            
+	            }
+	            });
+	    });
+		    
+	});
+ 
+};
