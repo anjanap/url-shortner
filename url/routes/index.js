1e1dd4eb9f7ac578824328e049cc73ffcba4d123
Anjana <anjanap1308@gmail.com>
2017-11-17 23:52:15 -0800

    redirection

--- url/routes/index.js
+++ url/routes/index.js
@@ -1,3 +1,14 @@
+var mongo = require("./mongo");
+var mongoURL = "mongodb://localhost:27017/hackaton";
+var shortUrl = require('node-url-shortener');
+var opn = require('opn');
+
 exports.index = function(req, res){
-	            	 res.render('index',{url:""}); 
+	mongo.connect(mongoURL, function(){
+var col = mongo.collection('url_details');
+		              col.find().toArray(function(err,list){
+		           	console.log("URL: "+list[0]._id);
+	            	 res.render('index',{url:"",urls:list}); 
+		              }
+		              )});
 };
